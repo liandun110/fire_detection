dd860bcae0f3510e97435b7317e716a08278bf91
zhbli <lizhenbang56@163.com>
2024-11-19 14:00:46 +0800

    减少对pytorch的使用

--- detect_onnx.py
+++ detect_onnx.py
@@ -99,10 +99,6 @@ def non_max_suppression(
     if isinstance(prediction, (list, tuple)):  # YOLOv5 model in validation model, output = (inference_out, loss_out)
         prediction = prediction[0]  # select only inference output
 
-    device = prediction.device
-    mps = "mps" in device.type  # Apple MPS
-    if mps:  # MPS not fully supported yet, convert tensors to CPU before NMS
-        prediction = prediction.cpu()
     bs = prediction.shape[0]  # batch size
     nc = prediction.shape[2] - nm - 5  # number of classes
     xc = prediction[..., 4] > conf_thres  # candidates
@@ -117,7 +113,7 @@ def non_max_suppression(
     merge = False  # use merge-NMS
 
     mi = 5 + nc  # mask start index
-    output = [torch.zeros((0, 6 + nm), device=prediction.device)] * bs
+    output = [np.zeros((0, 6 + nm))] * bs
     for xi, x in enumerate(prediction):  # image index, image inference
         # Apply constraints
         # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
@@ -143,49 +139,29 @@ def non_max_suppression(
         box = xywh2xyxy(x[:, :4])  # center_x, center_y, width, height) to (x1, y1, x2, y2)
         mask = x[:, mi:]  # zero columns if no masks
 
-        # Detections matrix nx6 (xyxy, conf, cls)
-        if multi_label:
-            i, j = (x[:, 5:mi] > conf_thres).nonzero(as_tuple=False).T
-            x = torch.cat((box[i], x[i, 5 + j, None], j[:, None].float(), mask[i]), 1)
-        else:  # best class only
-            conf, j = x[:, 5:mi].max(1, keepdim=True)
-            x = torch.cat((box, conf, j.float(), mask), 1)[conf.view(-1) > conf_thres]
+        conf = np.max(x[:, 5:mi], axis=1, keepdims=True)
+        j = np.argmax(x[:, 5:mi], axis=1, keepdims=True)
+        x = np.concatenate((box, conf, j.astype(np.float32), mask), axis=1)
+        conf_condition = conf > conf_thres
+        x = x[conf_condition.ravel()]
 
         # Filter by class
         if classes is not None:
             x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
 
-        # Apply finite constraint
-        # if not torch.isfinite(x).all():
-        #     x = x[torch.isfinite(x).all(1)]
-
         # Check shape
         n = x.shape[0]  # number of boxes
         if not n:  # no boxes
             continue
-        x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence and remove excess boxes
+        indices = np.argsort(-x[:, 4])  # 注意：np.argsort 返回升序索引，所以我们取相反数来得到降序
+        x = x[indices[:max_nms]]
 
         # Batched NMS
         c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
         boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
-        # i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
-        # boxes: tensor, shape=(28, 4). 28: 边框数量。4：(x1, y1, x2, y2)
-        # scores: tensor, shape=(28).
-        # iou_thres: 0.45
-        i = nms(boxes.numpy(), scores.numpy(), 0.45)
-
+        i = nms(boxes, scores, 0.45)
         i = i[:max_det]  # limit detections
-        if merge and (1 < n < 3e3):  # Merge NMS (boxes merged using weighted mean)
-            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
-            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
-            weights = iou * scores[None]  # box weights
-            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
-            if redundant:
-                i = i[iou.sum(1) > 1]  # require redundancy
-
         output[xi] = x[i]
-        if mps:
-            output[xi] = output[xi].to(device)
 
     return output
 
@@ -269,10 +245,8 @@ y = session.run(['output0'], {session.get_inputs()[0].name: im})[0]
 # y[..., 4]: 每个边框的objective置信度得分
 # y[..., 5:]: 每个边框的类别置信度
 
-pred = torch.from_numpy(y).to('cpu')
-
 # 应用非极大值抑制
-pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
+pred = non_max_suppression(y, 0.25, 0.45, None, False, max_det=1000)
 
 # 绘制检测框在原始图像 im0 上
 for i, det in enumerate(pred):
